7a36f9664ae2fd81d1dd210bc6641957077f60b4
Vick Kumar <vick.kumar44@gmail.com>
2018-03-20 09:48:04 -0400

    imported aux into component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,5 +1,16 @@
 import React, { Component } from 'react'
 
+import Aux from '../../hoc/aux'
+
 class BurgerBuilder extends Component{
-  
+  render(){
+    return (
+      <Aux>
+        <div>Burger</div>
+        <div>Build Controls</div>
+      </Aux>
+    )
+  }
 }
+
+export default BurgerBuilder;
