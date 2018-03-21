8c780785ca615fae94e2dd6f9c441b204a4bd38f
Vick Kumar <vick.kumar44@gmail.com>
2018-03-21 10:17:29 -0400

    created burger ingredient component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,14 +1,12 @@
-import React from 'react'
+import React from "react";
 
-import Aux from '../../hoc/Aux'
+import Aux from "../../hoc/Aux";
 
-const layout = (props) => (
+const layout = props => (
   <Aux>
     <div>Toolbar, SideDrawer, Backdrop</div>
-    <main>
-      {props.children}
-    </main>
+    <main>{props.children}</main>
   </Aux>
-)
+);
 
 export default layout;
